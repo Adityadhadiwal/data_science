df9dc73b21c22ed2b322271fd977fbc4ec4a16da
AdityaDhadiwal <aditya.18157@sakec.ac.in>
2025-08-12 02:19:21 +0530

    data ingestion

---
+++ src/logger.py
@@ -0,0 +1,16 @@
+import logging
+import os
+
+LOG_FILE = "running_logs.log"
+logs_path = os.path.join(os.getcwd(), "logs")
+os.makedirs(logs_path, exist_ok=True)
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s] %(levelname)s: %(message)s",
+    handlers=[
+        logging.FileHandler(LOG_FILE_PATH),   # Save to file
+        logging.StreamHandler()               # Print to terminal
+    ]
+)
